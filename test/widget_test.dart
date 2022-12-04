ec4ffdf7038a83bc09f409ecdddb0fe465492fd0
WahyuKurniaSenjaya <87650735+WahyuKurniaSenjaya@users.noreply.github.com>
2022-12-04 22:19:57 +0700

    commit conditional & function

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,47 +11,55 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_pertama/main.dart';
 
 void main() {
+    print('VARIAN');
     //var
-  var mahasiswa = "Satria Wahyu Kurniawan";
+  var mahasiswa = "Wahyu Kurnia Senjaya";
   var umur = "20";
 
   print(mahasiswa + " Usia = " + umur.toString());
 
   //string
+    print('STRING');
   String mahasiswaString = "SaWahKu";
 
   print(mahasiswaString);
 
   //int
+    print('INTEGER');
   int semester = 5;
 
   print(semester);
 
   //double
+    print('DOUBLE');
   double ipk = 3.9;
 
   print(ipk);
 
   //boolean
+    print('BOOLEAN');
   bool benar = true;
   bool salah = false;
   bool tidakbenar = false;
   bool tidaksalah = true;
 
   //List
+    print('LIST');
   List jurusan = ["Sistem Informasi", "DKV", "Teknik Informatika"];
 
   print(jurusan);
 
   //Map
-  Map kelas = {"Nama": "Satria Wahyu Kurniawan", "Kelas": "SI E-Bisnis 5"};
+    print('MAP');
+  Map kelas = {"Nama": "Wahyu Kurnia Senjaya", "Kelas": "SI E-Bisnis 5"};
 
   print(kelas);
   print(kelas['Nama']);
 
   //Map
+    print('MAP STRING DYNAMIC');
   Map<String, dynamic> kelas2 = {
-    "Nama": "Satria Wahyu Kurniawan",
+    "Nama": "Wahyu Kurnia Senjaya",
     "Kelas": "SI E-Bisnis 5",
   };
 
@@ -61,6 +69,7 @@ void main() {
 }
 
   //OPERATOR
+print('OPERATOR');
   int a, b;
   a = 9;
   b = 7;
@@ -76,4 +85,60 @@ void main() {
   print(a < b);
   print(a >= b);
   print(a <= b);
+
+
+  // conditional
+  print('CONDITIONAL');
+  var nilai;
+  nilai = 90;
+
+  if (nilai >= 85) {
+    print('A');
+  } else if (nilai <= 84 && nilai >= 76) {
+    print('B');
+  } else if (nilai <= 75 && nilai >= 60) {
+    print('C');
+  } else {
+    print('D');
+  }
+
+  nilai >= 60 ? print('Lulus') : print('Tidak Lulus');
+
+  //Function
+  print('FUNCTION');
+
+  hitungNilai();
+  hitungNilai1(75, 90);
+  hitungNilai2(mapel1: 70, mapel2: 80);
+  hitungNilai3(75, 75, 100);
+  hitungNilai4(75, 100);
+}
+
+//function
+hitungNilai() {
+  print('Hitung Nilai');
+}
+
+//positional argument
+hitungNilai1(mapel1, mapel2) {
+  print(mapel1 + mapel2);
+}
+
+//name argument
+hitungNilai2({mapel1, mapel2}) {
+  print(mapel1 + mapel2);
+}
+
+//optional argument
+hitungNilai3(mapel1, mapel2, [mapel3]) {
+  if (mapel3 != null) {
+    print(mapel1 + mapel2 + mapel3);
+  } else {
+    print(mapel1 + mapel2);
+  }
+}
+
+//void
+void hitungNilai4(mapel1, mapel2) {
+  print(mapel1 + mapel2);
 }
