7100e1feead49044e229eddcb9cde716bb413cdc
WahyuKurniaSenjaya <87650735+WahyuKurniaSenjaya@users.noreply.github.com>
2022-12-04 22:32:06 +0700

    commit oop, class, inheritance2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,27 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+//oop
+//class
+class Kendaraan {
+  String? merk;
+  String? nama;
+  int? harga;
+
+  //construktor
+  Kendaraan(this.merk, this.nama, this.harga);
+
+  //method
+  maju(int kecepatan) {
+    print(kecepatan.toString() + 'KM/Jam');
+  }
+}
+
+//Inheritance atau Pewarisan
+class Sedan extends Kendaraan {
+  int? jumlahPintu;
+  int? kecepatanMaksimal;
+
+  Sedan({String? merk, this.jumlahPintu, this.kecepatanMaksimal})
+      : super(merk = merk);
+}
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
